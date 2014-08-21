d43c1d73eaa92a1e932aa2469b08f63e7916be56
hushicai <bluthcy@gmail.com>
2014-08-21 17:23:31 +0800

    update

--- src/algorithm/Rotate.js
+++ src/algorithm/Rotate.js
@@ -5,7 +5,7 @@
 
 // 参考文章：http://blog.csdn.net/v_JULY_v/article/details/6322882
 // js实现
-// 其实我主要想记录一下各个交换过程，然后用动画演示一遍
+// 其实我主要是想记录一下字符移动过程，然后用动画演示一遍
 
 define(
     function(require) {
@@ -32,6 +32,7 @@ define(
             var p1 = 0;
             var p2 = m;
 
+            // 长度为m的子字符串可以向前整体移动的次数
             var k = (n - m) - n % m;
 
             while(k--) {
@@ -45,6 +46,8 @@ define(
                 p2++;
             }
 
+            // 剩余的字符串处理
+            // 挨个向左移动
             var r = n - p2;
             while(r--) {
                 var i = p2;
